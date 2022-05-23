ec0f89c9ca2a460bd5ff1d4fc5b52f174f5f6107
pickerX <pf.developer@outlook.com>
2022-05-23 16:39:48 +0800

    [ionic] add multi platform support

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vue3_ts_wx_template',
+  appName: 'vue3_ts_wx_template',
+  webDir: 'dist',
+  bundledWebRuntime: false
+};
+
+export default config;
